eec701eaa545bb2ae558ac3692f87e7f32aec262
Виктор Михайлов <v.mikhailov@yottapay.co.uk>
2022-11-29 15:49:20 +0300

    bump version

--- src/main/java/com/yotta/sdk/payment/config/YpSdkPaymentConfiguration.java
+++ src/main/java/com/yotta/sdk/payment/config/YpSdkPaymentConfiguration.java
@@ -9,7 +9,7 @@ import static com.yotta.sdk.core.config.YpSdkConfiguration.propertyPath;
 
 public interface YpSdkPaymentConfiguration extends YpSdkConfiguration {
 
-    String SDK_NAME = "Yotta Pay SDK (Payments) (1.0.8)";
+    String SDK_NAME = "Yotta Pay SDK (Payments) (1.0.9)";
 
     static YpSdkPaymentConfiguration createDefault() {
         return new YpSdkPaymentConfigurationImpl();
